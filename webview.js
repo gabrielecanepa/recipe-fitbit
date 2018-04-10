8b717df75d0c21167755fb003f55bd48a04f702f
Gabriele Canepa <gabriele@canepa.io>
2018-04-10 02:33:30 +0100

    Uncomment previous script

--- webview.js
+++ webview.js
@@ -1,13 +1,13 @@
-// module.exports = Franz => {
-//   let momentJS = document.createElement('script');
-//   momentJS.src = 'https://www.fitbit.com/scripts/libs/moment/moment.js';
-//   document.head.appendChild(momentJS);
+module.exports = Franz => {
+  let momentJS = document.createElement('script');
+  momentJS.src = 'https://www.fitbit.com/scripts/libs/moment/moment.js';
+  document.head.appendChild(momentJS);
 
-//   let safeUnescape = document.createElement('script');
-//   safeUnescape.src = 'https://www.fitbit.com/scripts/galileo/util/safeUnescape.js';
-//   document.head.appendChild(safeUnescape);
+  let safeUnescape = document.createElement('script');
+  safeUnescape.src = 'https://www.fitbit.com/scripts/galileo/util/safeUnescape.js';
+  document.head.appendChild(safeUnescape);
 
-//   let googleAnalytics = document.createElement('script');
-//   googleAnalytics.src = 'https://www.fitbit.com/scripts/galileo/tracking/google_analytics.js';
-//   document.head.appendChild(googleAnalytics);
-// };
+  let googleAnalytics = document.createElement('script');
+  googleAnalytics.src = 'https://www.fitbit.com/scripts/galileo/tracking/google_analytics.js';
+  document.head.appendChild(googleAnalytics);
+};
