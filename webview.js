7078a6e26fab8d100c0d49a757046132532ce137
Gabriele Canepa <gabriele@canepa.io>
2018-04-10 16:53:23 +0100

    Remove galielo app and small changes

--- webview.js
+++ webview.js
@@ -1,10 +1,9 @@
-"use strict";
+'use strict';
 
 module.exports = Franz => {
-  // Load galileo
-  let galieloApp = document.createElement('script');
-  galieloApp.src = 'https://assets.fitbit.com/production/app.lab.galileo.base.6.js.e3aec34a47a604a3bc03b95f9fafa38b.js';
-  document.head.appendChild(galieloApp);
+  // let galieloApp = document.createElement('script');
+  // galieloApp.src = 'https://assets.fitbit.com/production/app.lab.galileo.base.6.js.e3aec34a47a604a3bc03b95f9fafa38b.js';
+  // document.head.appendChild(galieloApp);
 
   document.querySelector(".store").remove();
   document.querySelector("footer").remove();
