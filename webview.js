dedf2e5c3475ef0c67bbc752c6f5e240ade368c7
Gabriele Canepa <gabriele@canepa.io>
2018-04-10 01:38:37 +0100

    Update README.md

--- webview.js
+++ webview.js
@@ -1,1 +1,13 @@
-"use strict";
+// module.exports = Franz => {
+//   let momentJS = document.createElement('script');
+//   momentJS.src = 'https://www.fitbit.com/scripts/libs/moment/moment.js';
+//   document.head.appendChild(momentJS);
+
+//   let safeUnescape = document.createElement('script');
+//   safeUnescape.src = 'https://www.fitbit.com/scripts/galileo/util/safeUnescape.js';
+//   document.head.appendChild(safeUnescape);
+
+//   let googleAnalytics = document.createElement('script');
+//   googleAnalytics.src = 'https://www.fitbit.com/scripts/galileo/tracking/google_analytics.js';
+//   document.head.appendChild(googleAnalytics);
+// };
